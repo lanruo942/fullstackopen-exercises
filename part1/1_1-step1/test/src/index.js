7e6a7e9eebb932552483c631bda262af49cd4570
Summer Lee <lee@summer.today>
2021-07-08 17:18:04 +0800

    complete part1/1_1

---
+++ part1/1_1-step1/test/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Summer Lee
+ * @Date: 2021-07-08 16:48:05
+ * @LastEditors: Summer Lee
+ * @LastEditTime: 2021-07-08 16:53:16
+ */
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
