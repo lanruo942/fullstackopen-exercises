46febd2a3988910fb5c8269372ced3eb25e156e9
Summer Lee <lee@summer.today>
2022-03-04 18:20:53 +0800

    complete part1/1.12

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,46 @@
+/*
+ * @Author: Summer Lee
+ * @Date: 2022-03-04 16:09:06
+ * @LastEditors: Summer Lee
+ * @LastEditTime: 2022-03-04 18:19:38
+ */
+import React, { useState } from 'react'
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const randomNum = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  const setToValue = (value) => {
+    setSelected(value)
+  }
+
+  return (
+    <>
+      {anecdotes[selected]}
+      <br />
+      <Button handleClick={() => setToValue(randomNum(0, anecdotes.length))} text="next anecdote" />
+    </>
+  )
+}
+
+export default App
---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Summer Lee
+ * @Date: 2022-03-04 16:09:06
+ * @LastEditors: Summer Lee
+ * @LastEditTime: 2022-03-04 17:49:08
+ */
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+)
