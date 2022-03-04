be1c8dd8ee6d3bfde81bfab08012b580ce5d5282
Summer Lee <lee@summer.today>
2022-03-04 15:31:37 +0800

    complete part1/1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,7 @@
  * @Author: Summer Lee
  * @Date: 2022-03-04 10:28:50
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-03-04 14:39:03
+ * @LastEditTime: 2022-03-04 14:51:55
  */
 import React, { useState } from 'react'
 
@@ -14,14 +14,23 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Display = ({ good, neutral, bad }) => (
-  <div>
-    <h2>Statistics</h2>
-    <p>good {good}</p> 
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-)
+const Display = ({ good, neutral, bad }) => {
+  const all = (good + neutral + bad) ? (good + neutral + bad) : 0
+  const average = ((good - bad)/all) ? ((good - bad)/all) : 0
+  const positive = (good/all) ? (good/all + '%') : '0%'
+  
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {good}</p> 
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </div>
+  )
+}
 
 const App = () => {
   const [ good, setGood ] =useState(0)
