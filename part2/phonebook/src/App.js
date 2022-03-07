630270f1be13caef0eb08e6213a3e2c6f769e061
Summer Lee <lee@summer.today>
2022-03-07 14:38:52 +0800

    complete part2/2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,7 @@
  * @Author: Summer Lee
  * @Date: 2022-03-06 16:24:41
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-03-07 13:18:29
+ * @LastEditTime: 2022-03-07 14:37:24
  */
 import React, { useState } from 'react'
 import { nanoid } from 'nanoid'
@@ -12,15 +12,27 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       id: nanoid(),
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: '040-1234567'
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     
     const trimName = newName.trim()
+    const trimNumber = newNumber.trim()
+
+    if (trimName === '') {
+      return window.alert('Please enter your name.')
+    }
+
+    if (trimNumber === '') {
+      return window.alert('Please enter your number.')
+    }
+
     const isExist = persons.find(element => {
       return element.name === trimName
     })
@@ -30,11 +42,13 @@ const App = () => {
     } else {
       const nameObject = {
         id: nanoid(),
-        name: trimName
+        name: trimName,
+        number: trimNumber
       }
 
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -42,16 +56,17 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <table>
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -2,13 +2,14 @@
  * @Author: Summer Lee
  * @Date: 2022-03-07 12:33:35
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-03-07 13:03:04
+ * @LastEditTime: 2022-03-07 14:31:39
  */
 import React from 'react'
 
 const Person = ({ person }) => (
 	<tr>
 		<td>{person.name}</td>
+		<td>{person.number}</td>
 	</tr>
 )
 
