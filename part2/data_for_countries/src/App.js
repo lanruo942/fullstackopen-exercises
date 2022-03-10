882fb588968ec69a8b5deabc7e1f346e730fb628
Summer Lee <lee@summer.today>
2022-03-10 23:17:15 +0800

    complete part2/2.14

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -2,7 +2,7 @@
  * @Author: Summer Lee
  * @Date: 2022-03-09 17:07:22
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-03-10 19:41:44
+ * @LastEditTime: 2022-03-11 14:39:09
  */
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
@@ -35,6 +35,49 @@ const DisplayCountry = ({ country }) => {
   )
 }
 
+const DisplayWeather = ({ city }) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [weathers, setWeathers] = useState({
+    temp: 0,
+    icon: '',
+    wind: '' 
+  })
+
+  useEffect(() => {
+    axios
+      .get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${api_key}`)
+      .then(response => {
+        const lat = response.data[0].lat
+        const lon = response.data[0].lon
+
+        axios
+          .get(`http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${api_key}`)
+          .then(response => {
+            if (Object.keys(response.data).length) {
+              const weathersObj = {}
+              weathersObj['temp'] = (5 / 9 * (response.data.main.temp - 32)).toFixed(2)
+              weathersObj['icon'] = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+              weathersObj['wind'] = `${response.data.wind.speed} mph degrees ${response.data.wind.deg}`
+
+              setWeathers(weathersObj)
+
+              // Prevent memory leaks
+              return () => setWeathers({})
+            }
+          })
+      })
+  }, [api_key, city])
+
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <p>temperature: {weathers.temp} Celsius</p>
+      <img src={weathers.icon} alt="" />
+      <p>wind: {weathers.wind}</p>
+    </div>
+  )
+}
+
 const DisplayLine = ({ result, handleShow }) => (
   <div>
     {result.name.common}
@@ -50,7 +93,10 @@ const Display = ({ results, handleShow }) => {
     )
   } else if (reLength === 1) {
     return (
-      <DisplayCountry country={results[0]} />
+      <div>
+        <DisplayCountry country={results[0]} />
+        <DisplayWeather city={results[0].capital} />
+      </div>
     )
   } else {
     return (
@@ -75,15 +121,13 @@ const App = () => {
   const [results, setResults] = useState([])
   const [newSearch, setNewSearch] = useState('')
 
-  const getCountries = () => {
+  useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         setCountries(response.data)
       })
-  }
-
-  useEffect(getCountries, [])
+  }, [])
   
   const handleSearch = (event) => {    
     const search = event.target.value.trim().toLowerCase()
