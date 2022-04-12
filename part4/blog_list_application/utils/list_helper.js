de30caf93b8df456e97140f9aa1d5f3b53082eb7
Summer Lee <lee@summer.today>
2022-04-12 16:13:48 +0800

    complete part4/4.4

--- part4/blog_list_application/utils/list_helper.js
+++ part4/blog_list_application/utils/list_helper.js
@@ -2,12 +2,19 @@
  * @Author: Summer Lee
  * @Date: 2022-04-10 21:26:24
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-04-11 15:24:51
+ * @LastEditTime: 2022-04-12 16:09:31
  */
 const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	return blogs.reduce((prev, next) => {
+		return prev + next.likes
+	}, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
