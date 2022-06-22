744eed684b44704ceb790d641acda636a51db74f
Summer Lee <lee@summer.today>
2022-06-23 04:10:24 +0800

    complete part5/5.7

--- part5/bloglist-frontend/src/components/Blogs/Blog.js
+++ part5/bloglist-frontend/src/components/Blogs/Blog.js
@@ -2,12 +2,34 @@
  * @Author: Summer Lee
  * @Date: 2022-06-17 03:13:07
  * @LastEditors: Summer Lee
- * @LastEditTime: 2022-06-17 18:21:16
+ * @LastEditTime: 2022-06-23 04:09:36
  */
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+import './Blog.css'
+
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonStatus, setButtonStatus] = useState('view')
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+    setButtonStatus(buttonStatus === 'view' ? 'hide' : 'view')
+  }
+
+  return (
+    <div className="blog-style">
+      {blog.title}&nbsp;
+      <button onClick={toggleVisibility}>{buttonStatus}</button>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
