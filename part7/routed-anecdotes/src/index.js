8ddd425707a1eeb921c3943d38e5827a3e9ab556
Summer Lee <lee@summer.today>
2022-07-08 17:27:28 +0800

    complete part7/7.1

---
+++ part7/routed-anecdotes/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Summer Lee
+ * @Date: 2022-07-08 03:00:03
+ * @LastEditors: Summer Lee
+ * @LastEditTime: 2022-07-08 16:12:19
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Router>
+		<App />
+	</Router>
+)
