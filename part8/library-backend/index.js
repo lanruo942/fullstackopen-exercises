2df3a984cfaadb31872ae19172d4908028345046
Summer Lee <lee@summer.today>
2022-07-15 18:30:22 +0800

    complete part8/8.5

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
-		allBooks(author: String!): [Book!]!
+		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
 	}
 `
@@ -117,11 +117,20 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: (root, args) => {
-			if (!args.author) {
+			if (!args.author && !args.genre) {
 				return books
 			}
+			if (args.author && !args.genre) {
+				return books.filter((book) => book.author === args.author)
+			}
+			if (!args.author && args.genre) {
+				return books.filter((book) => book.genres.includes(args.genre))
+			}
 
-			return books.filter((book) => book.author === args.author)
+			return books.filter(
+				(book) =>
+					book.author === args.author && book.genres.includes(args.genre)
+			)
 		},
 		allAuthors: () => {
 			return authors.map((author) => {
