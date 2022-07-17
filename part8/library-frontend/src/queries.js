4bb2c31d827c78ee10bff795d68d22465c6c5098
Summer Lee <lee@summer.today>
2022-07-18 03:33:04 +0800

    complete part8/8.10

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -2,7 +2,7 @@
  * @Author: Summer Lee
  * @Date: 2022-07-18 03:00:09
  * @LastEditors: Summer Lee lee@summer.today
- * @LastEditTime: 2022-07-18 03:14:35
+ * @LastEditTime: 2022-07-18 03:19:38
  */
 import { gql } from '@apollo/client'
 
@@ -20,6 +20,27 @@ export const ALL_AUTHORS = gql`
 export const ALL_BOOKS = gql`
 	query AllBooks {
 		allBooks {
+			title
+			published
+			author
+			id
+		}
+	}
+`
+
+export const CREATE_BOOK = gql`
+	mutation AddBook(
+		$title: String!
+		$author: String!
+		$published: Int
+		$genres: [String!]
+	) {
+		addBook(
+			title: $title
+			author: $author
+			published: $published
+			genres: $genres
+		) {
 			title
 			published
 			author
